e60d8f196dc376804ef6abbe000fd089b5b3d07d
Christopher Aitken <chris@heythisischris.com>
2025-01-26 14:38:15 -0600

    Renamed services table, returning empty arrays if no joined rows found

--- routes/auth/guest/getGuests.ts
+++ routes/auth/guest/getGuests.ts
@@ -6,14 +6,18 @@ export const getGuests = async () => {
         text: `
             SELECT CAST(COUNT(g.guest_id) AS INT) AS total_guests, g.*,
             jsonb_agg(DISTINCT to_jsonb(gn.*)) FILTER (WHERE gn.notification_id IS NOT NULL) as notifications,
-            jsonb_agg(DISTINCT to_jsonb(gs.*)) FILTER (WHERE gs.service_id IS NOT NULL) as services
+            jsonb_agg(DISTINCT to_jsonb(s.*)) FILTER (WHERE s.service_id IS NOT NULL) as services
             FROM "guests" g
             LEFT JOIN "guest_notifications" gn ON gn.guest_id = g.guest_id
-            LEFT JOIN "guest_services" gs ON gs.guest_id = g.guest_id 
+            LEFT JOIN "services" s ON s.guest_id = g.guest_id 
             GROUP BY g.guest_id;
         `,
         values: [],
-    })).rows;
+    })).rows.map(row => ({
+        ...row,
+        notifications: row?.notifications ?? [],
+        services: row?.services ?? [],
+    }));
     await db.clean();
     return rows;
 }
