52abafcbe03b86146a94882802572f85951502d7
Christopher Aitken <chris@heythisischris.com>
2025-01-26 14:09:27 -0600

    Rolling joined service/notification arrays into getGuests response

--- routes/auth/guest/getGuests.ts
+++ routes/auth/guest/getGuests.ts
@@ -3,7 +3,15 @@ import { db, event } from '#src/utils'
 export const getGuests = async () => {
     await db.connect();
     const rows = (await db.query({
-        text: `SELECT * FROM "guests"`,
+        text: `
+            SELECT CAST(COUNT(g.guest_id) AS INT) AS total_guests, g.*,
+            jsonb_agg(DISTINCT to_jsonb(gn.*)) FILTER (WHERE gn.notification_id IS NOT NULL) as notifications,
+            jsonb_agg(DISTINCT to_jsonb(gs.*)) FILTER (WHERE gs.service_id IS NOT NULL) as services
+            FROM "guests" g
+            LEFT JOIN "guest_notifications" gn ON gn.guest_id = g.guest_id
+            LEFT JOIN "guest_services" gs ON gs.guest_id = g.guest_id 
+            GROUP BY g.guest_id;
+        `,
         values: [],
     })).rows;
     await db.clean();
