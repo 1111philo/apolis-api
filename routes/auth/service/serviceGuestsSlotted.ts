db6c8461b389c0e61db08d1900d7a23e0d02176a
Blake Bertuccelli-Booth <46652+bbertucc@users.noreply.github.com>
2025-01-29 11:27:22 -0600

    Return slot ID

--- routes/auth/service/serviceGuestsSlotted.ts
+++ routes/auth/service/serviceGuestsSlotted.ts
@@ -1,15 +1,30 @@
-import { db, event } from '#src/utils'
+import { db, event } from '#src/utils';
 
-export const serviceGuestsSlotted = async () => {
+export const serviceGuestsSlotted = async ({ service_id = null, guest_id = null } = {}) => {
     await db.connect();
-    const rows = (await db.query({
+    
+    const query = {
         text: `
-            SELECT "guests".* FROM "guests" 
-            INNER JOIN "guest_services" ON "guests"."guest_id" = "guest_services"."guest_id"
-            WHERE "guest_services"."status"=$1
+            SELECT 
+                guests.guest_id,
+                guests.first_name,
+                guests.last_name,
+                guests.dob,
+                guests.case_manager,
+                guest_services.service_id,
+                guest_services.slot_id AS slot_number, -- Include slot ID
+                guest_services.status,
+                guest_services.queued_at
+            FROM guests
+            INNER JOIN guest_services ON guests.guest_id = guest_services.guest_id
+            WHERE guest_services.status = $1
+            ${service_id ? "AND guest_services.service_id = $2" : ""}
+            ${guest_id ? "AND guests.guest_id = $3" : ""}
         `,
-        values: ['Slotted'],
-    })).rows;
+        values: ['Slotted', ...(service_id ? [service_id] : []), ...(guest_id ? [guest_id] : [])]
+    };
+
+    const rows = (await db.query(query)).rows;
     await db.clean();
     return rows;
-}
+};
