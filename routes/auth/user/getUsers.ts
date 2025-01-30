1b6df76b6ec8a06d28dee1049f8058c41f7856b5
Shiraj <shirajganguly@gmail.com>
2025-01-30 08:06:48 -0600

    Add sortBy

--- routes/auth/user/getUsers.ts
+++ routes/auth/user/getUsers.ts
@@ -4,11 +4,12 @@ export const getUsers = async () => {
   const limit = event.queryStringParameters?.limit ?? 10;
   const offset = event.queryStringParameters?.page ?? 0;
   const sort = event.queryStringParameters?.sort === "desc" ? "DESC" : "ASC";
+  const sortBy = event.queryStringParameters?.sortBy ?? "id";
 
   await db.connect();
   const rows = (
     await db.query({
-      text: `SELECT * FROM "users" ORDER BY id ${sort} LIMIT $1 OFFSET $2`,
+      text: `SELECT * FROM "users" ORDER BY ${sortBy} ${sort} LIMIT $1 OFFSET $2`,
       values: [limit, offset],
     })
   ).rows;
