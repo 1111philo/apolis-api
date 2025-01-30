953f2b543131e7c4e89a7d7f8c917baa2c651aca
Shiraj <shirajganguly@gmail.com>
2025-01-30 11:16:28 -0600

    Update other endpoints and unify

--- routes/auth/guest/getGuests.ts
+++ routes/auth/guest/getGuests.ts
@@ -1,28 +1,31 @@
-import { db, event } from '#src/utils'
+import { db, event } from "#src/utils";
 
 export const getGuests = async () => {
-    const limit = event.body?.limit ?? 10;
-    const offset = event.body?.offset ?? 0;
-    let first_name = event.body?.first_name && `%${event.body.first_name}%`;
-    let last_name = event.body?.last_name && `%${event.body.last_name}%`;
-    let dob = event.body?.dob && `%${event.body.dob}%`;
-    let guest_id = event.body?.guest_id;
-    const query = event.body?.query && `%${event.body.query}%`;
+  const limit = event.body?.limit ?? 10;
+  const offset = event.body?.offset ?? 0;
+  let first_name = event.body?.first_name && `%${event.body.first_name}%`;
+  let last_name = event.body?.last_name && `%${event.body.last_name}%`;
+  let dob = event.body?.dob && `%${event.body.dob}%`;
+  let guest_id = event.body?.guest_id;
+  const query = event.body?.query && `%${event.body.query}%`;
+  const sort = event.body?.sort === "desc" ? "DESC" : "ASC";
+  const sortBy = event.body?.sortBy ?? "id";
 
-    if (query) {
-        first_name = query;
-        last_name = query;
-        dob = query;
-        guest_id = query;
-    }
+  if (query) {
+    first_name = query;
+    last_name = query;
+    dob = query;
+    guest_id = query;
+  }
 
-    await db.connect();
-    const rows = (await db.query({
-        text: `
+  await db.connect();
+  const rows = (
+    await db.query({
+      text: `
             SELECT g.*, g.dob::text, CAST(COUNT(*) OVER() AS INT) AS total
             FROM "guests" g
             WHERE (
-                CASE 
+                CASE
                     WHEN $1::text IS NOT NULL OR $2::text IS NOT NULL OR $3::text IS NOT NULL THEN
                         ($1::text IS NULL OR g.first_name ILIKE $1)
                         OR ($2::text IS NULL OR g.last_name ILIKE $2)
@@ -31,20 +34,30 @@ export const getGuests = async () => {
                     ELSE TRUE
                 END
             )
-            ORDER BY g.guest_id
+            ORDER BY $7 $8
             LIMIT $5 OFFSET $6
         `,
-        values: [first_name, last_name, dob, guest_id, limit, offset],
-    })).rows;
-    await db.clean();
-
-    return {
-        rows: rows?.map(row => {
-            delete row.total;
-            return row;
-        }),
-        total: rows?.[0]?.total ?? 0,
+      values: [
+        first_name,
+        last_name,
+        dob,
+        guest_id,
         limit,
         offset,
-    };
-}
+        sortBy,
+        sort,
+      ],
+    })
+  ).rows;
+  await db.clean();
+
+  return {
+    rows: rows?.map((row) => {
+      delete row.total;
+      return row;
+    }),
+    total: rows?.[0]?.total ?? 0,
+    limit,
+    offset,
+  };
+};
--- routes/auth/user/getUsers.ts
+++ routes/auth/user/getUsers.ts
@@ -2,15 +2,15 @@ import { db, event } from "#src/utils";
 
 export const getUsers = async () => {
   const limit = event.queryStringParameters?.limit ?? 10;
-  const offset = event.queryStringParameters?.page ?? 0;
+  const offset = event.queryStringParameters?.offset ?? 0;
   const sort = event.queryStringParameters?.sort === "desc" ? "DESC" : "ASC";
   const sortBy = event.queryStringParameters?.sortBy ?? "id";
 
   await db.connect();
   const rows = (
     await db.query({
-      text: `SELECT * FROM "users" ORDER BY ${sortBy} ${sort} LIMIT $1 OFFSET $2`,
-      values: [limit, offset],
+      text: `SELECT * FROM "users" ORDER BY $3 $4 LIMIT $1 OFFSET $2`,
+      values: [limit, offset, sortBy, sort],
     })
   ).rows;
   await db.clean();
