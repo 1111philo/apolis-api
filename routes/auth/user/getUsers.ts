503b401bcad043422dc68df2f756684bee26c9e8
Shiraj <shirajganguly@gmail.com>
2025-01-30 07:54:22 -0600

    Add sort direction

--- routes/auth/user/getUsers.ts
+++ routes/auth/user/getUsers.ts
@@ -3,11 +3,12 @@ import { db, event } from "#src/utils";
 export const getUsers = async () => {
   const limit = event.queryStringParameters?.limit ?? 10;
   const offset = event.queryStringParameters?.page ?? 0;
+  const sort = event.queryStringParameters?.sort === "desc" ? "DESC" : "ASC";
 
   await db.connect();
   const rows = (
     await db.query({
-      text: `SELECT * FROM "users" LIMIT $1 OFFSET $2`,
+      text: `SELECT * FROM "users" ORDER BY id ${sort} LIMIT $1 OFFSET $2`,
       values: [limit, offset],
     })
   ).rows;
