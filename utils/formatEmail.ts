638e3f9c352b59307ae4e5f6e4984a4df9dada0b
Christopher Aitken <chris@heythisischris.com>
2025-02-11 12:58:25 -0600

    Updated reset password email

--- utils/formatEmail.ts
+++ utils/formatEmail.ts
@@ -1,5 +1,5 @@
 export const formatEmail = ({ event, body }) => {
-    return `<p><img style="width:200px;" src="https://${process.env.URL}/email.jpg"></p>
+    return `<p><img style="width:200px;" src="https://${process.env.URL}/img/htic-logo.png"></p>
     Hey ${event?.request?.userAttributes?.name ?? 'user'},
     <p>${body}</p>
     <p>Thanks,
