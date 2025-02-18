b97b02c3aae7b140964fe5f074547496835aedef
Shiraj <shirajganguly@gmail.com>
2025-02-18 16:16:54 -0600

    Update services to have a queueable column

--- routes/auth/service/updateService.ts
+++ routes/auth/service/updateService.ts
@@ -1,12 +1,12 @@
-import { db, event } from '#src/utils'
+import { db, event } from '#src/utils';
 
 export const updateService = async () => {
-    const { name, quota, service_id } = event.body;
-    await db.connect();
-    await db.query({
-        text: `UPDATE "services" SET "name"=$1, "quota"=$2, "updated_at"=$3 WHERE "service_id"=$4`,
-        values: [name, quota, new Date().toISOString(), service_id],
-    });
-    await db.clean();
-    return { success: true };
-}
+  const { name, quota, queueable, service_id } = event.body;
+  await db.connect();
+  await db.query({
+    text: `UPDATE "services" SET "name"=$1, "quota"=$2, "queueable"=$3,"updated_at"=$4 WHERE "service_id"=$5`,
+    values: [name, quota, queueable, new Date().toISOString(), service_id],
+  });
+  await db.clean();
+  return { success: true };
+};
