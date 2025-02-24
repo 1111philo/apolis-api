22ab64ecd783c07fd223e8ba2f2952afd4e49453
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2025-02-24 17:34:55 -0600

    change: add whole guest obj to /updateGuest response

--- routes/auth/guest/updateGuest.ts
+++ routes/auth/guest/updateGuest.ts
@@ -3,10 +3,10 @@ import { db, event } from '#src/utils'
 export const updateGuest = async () => {
     const { first_name, last_name, dob, case_manager, guest_id } = event.body;
     await db.connect();
-    await db.query({
-        text: `UPDATE "guests" SET "first_name"=$1, "last_name"=$2, "dob"=$3, "case_manager"=$4, "updated_at"=$5 WHERE "guest_id"=$6`,
+    const guest = (await db.query({
+        text: `UPDATE "guests" SET "first_name"=$1, "last_name"=$2, "dob"=$3, "case_manager"=$4, "updated_at"=$5 WHERE "guest_id"=$6 RETURNING *`,
         values: [first_name, last_name, dob, case_manager, new Date().toISOString(), guest_id],
-    });
+    })).rows?.[0];
     await db.clean();
-    return { success: true };
+    return { success: true, guest };
 }
