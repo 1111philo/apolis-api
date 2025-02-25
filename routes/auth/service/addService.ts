6ff2cd8035990de828be8c42e4e6adcfa8f0c8e0
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2025-02-24 18:35:31 -0600

    change: add whole service obj to /addService response

--- routes/auth/service/addService.ts
+++ routes/auth/service/addService.ts
@@ -3,10 +3,10 @@ import { db, event } from '#src/utils'
 export const addService = async () => {
     const { name, quota } = event.body;
     await db.connect();
-    const service_id = (await db.query({
-        text: `INSERT INTO "services" ("name", "quota") VALUES ($1, $2) RETURNING "service_id"`,
+    const service = (await db.query({
+        text: `INSERT INTO "services" ("name", "quota") VALUES ($1, $2) RETURNING *`,
         values: [name, quota],
-    })).rows?.[0]?.service_id;
+    })).rows?.[0];
     await db.clean();
-    return { service_id };
+    return { service, service_id: service.service_id};
 }
